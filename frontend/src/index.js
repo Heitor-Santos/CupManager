cfc6303e0a41fc163341b5b446ad777fc180ee7d
Heitor <hss2@cin.ufpe.br>
2020-05-25 19:31:41 -0300

    inicio do ionic aí

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import Cup from './Cup/Cup';
-import Timer from './Partida/Timer';
-import Partida from './Partida/Partida';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import { IonApp } from '@ionic/react';
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-          <Route path="/" exact={true} component={App}/>
-          <Route path="/:nameCup" exact={true} component={Cup}/>
-          <Route path="/:nameCup/:namePartida" exact={true} component={Partida}/>
-          <Route path="/a/b/c" exact={true} component={Timer}/>
-        </Switch>
-    </BrowserRouter>,
-  document.getElementById('root')
+    <IonApp>
+        <App />
+    </IonApp>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
