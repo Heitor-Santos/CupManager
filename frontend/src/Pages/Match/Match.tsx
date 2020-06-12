69d9090a24c8290548b81a4a58ba11d469ef1128
Heitor <hss2@cin.ufpe.br>
2020-06-12 01:18:23 -0300

    morte ao firebase

--- frontend/src/Pages/Match/Match.tsx
+++ frontend/src/Pages/Match/Match.tsx
@@ -5,7 +5,7 @@ import Header from './Header'
 import TeamCard from './TeamCard'
 import ClockOptions from './ClockOptions'
 import Statics from './Statics'
-
+import { getCup, postMatch, getMatch, getPlayer, getPlayers } from '../../firebase/firestore'
 const slideOpts = {
     initialSlide: 0,
     speed: 400,
@@ -25,52 +25,57 @@ interface State {
     infoPlayers: Array<Array<infoPlayer>>
     matchState: string,
     matchTitle: string,
-    matchTime: string|undefined,
+    matchTime: string | undefined,
     gols: Array<number>,
     currGoleiros: Array<number>
 }
-interface Props{
-
+interface Props {
+    match: {
+        params: {
+            cupName: string,
+            matchName: string
+        }
+    }
 }
-class Match extends React.Component<Props,State> {
+interface MatchData {
+    teamA: Array<string>,
+    teamB: Array<string>,
+    matchState: string,
+    matchName: string,
+    matchTime: string,
+    gols: Array<number>,
+    currGoleiros: Array<number>
+}
+class Match extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
         this.state = {
             teams: [[], []], //lista de nomes de jogadores
             showAlert: false, //decide se abre a caixa de novo jogador
             infoPlayers: [[], []], //lista de informações sobre os jogadores
-            matchState: "", //estado atual da partida, NOT-BEGUN, BEGUN OU FINISHED
-            matchTitle: "",
+            matchState: "NOT-BEGUN", //estado atual da partida, NOT-BEGUN, BEGUN OU FINISHED
+            matchTitle: this.props.match.params.matchName,
             matchTime: undefined,
-            gols:[0,0],
-            currGoleiros:[0,0]
+            gols: [0, 0],
+            currGoleiros: [0, 0]
         }
     }
-    componentDidMount() { //apenas jogando valores quaisquer de teste
-        const teams = [
-            ['Heitor', 'Tiago', 'Gilmar', 'Elisson', 'Felipe'],
-            ['João', 'Lucas', 'Marcelo', 'Artur', 'José']
-        ]
-        const infoPlayers = teams.map((team) => {
-            return (team.map((player) => {
-                return ({
-                    name: player,
-                    isGoleiro: false,
-                    assist: Math.floor(Math.random() * 10),
-                    golsFavor: Math.floor(Math.random() * 10),
-                    golsContra: Math.floor(Math.random() * 10),
-                    golsTomados: Math.floor(Math.random() * 10)
-                })
-            }))
-        })
-        this.setState({
-            teams: teams,
-            infoPlayers: infoPlayers,
-            matchState: "BEGUN",
-            matchTitle: "Partida 1",
-            gols: [0,0],
-            currGoleiros: [0,0]
-        })
+    async componentDidMount() {
+        let match: undefined | MatchData;
+        const cupName = this.props.match.params.cupName;
+        const matchName = this.props.match.params.matchName;
+        match = await getMatch(cupName, matchName) as MatchData|undefined
+        if (typeof match != 'undefined') {
+            const teams = [match.teamA, match.teamB]
+            const infoPlayers = await getPlayers(cupName, 'listOfMatches', 'array-contains', matchName)
+            this.setState({
+                teams: teams,
+                infoPlayers: infoPlayers,
+                matchState: match.matchState,
+                gols: match.gols,
+                currGoleiros: match.currGoleiros
+            })
+        }
     }
     /**
      * 
@@ -82,31 +87,31 @@ class Match extends React.Component<Props,State> {
         teams[currTeam].push(playerName)
         this.setState({ teams: teams })
     }
-    changePlayer(currTeam:number, indexPlayer:number, opt: keyof infoPlayer) {
+    changePlayer(currTeam: number, indexPlayer: number, opt: keyof infoPlayer) {
         let infoPlayers = this.state.infoPlayers
         let currGoleiros = this.state.currGoleiros;
         let gols = this.state.gols
-        if (opt == "isGoleiro"){
+        if (opt == "isGoleiro") {
             infoPlayers[currTeam][currGoleiros[currTeam]][opt] = false
             infoPlayers[currTeam][indexPlayer][opt] = true
             currGoleiros[currTeam] = indexPlayer
-        }            
-        else{
+        }
+        else {
             infoPlayers[currTeam][indexPlayer][opt]++
-            if(opt == "golsFavor"){
-                infoPlayers[currTeam==0?1:0][currGoleiros[currTeam==0?1:0]]['golsTomados']++
+            if (opt == "golsFavor") {
+                infoPlayers[currTeam == 0 ? 1 : 0][currGoleiros[currTeam == 0 ? 1 : 0]]['golsTomados']++
                 gols[currTeam]++
             }
-            if(opt == "golsContra"){
+            if (opt == "golsContra") {
                 infoPlayers[currTeam][currGoleiros[currTeam]]['golsTomados']++
-                gols[currTeam==0?1:0]++
+                gols[currTeam == 0 ? 1 : 0]++
             }
-        } 
-        this.setState({ infoPlayers,currGoleiros, gols})
+        }
+        this.setState({ infoPlayers, currGoleiros, gols })
     }
-    removePlayer(currTeam:number, indexPlayer:number){
+    removePlayer(currTeam: number, indexPlayer: number) {
         let teams = this.state.teams
-        teams[currTeam].splice(indexPlayer,1)
+        teams[currTeam].splice(indexPlayer, 1)
         this.setState({ teams: teams })
     }
     render() {
@@ -118,27 +123,27 @@ class Match extends React.Component<Props,State> {
         return (
             <div style={{}}>
                 <Toolbar title={this.state.matchTitle} />
-                <Header matchTime={this.state.matchTime} gols={this.state.gols}/>
+                <Header matchTime={this.state.matchTime} gols={this.state.gols} />
                 <IonSlides options={slideOpts}>
                     <IonSlide>
                         <TeamCard team="Time A" players={players[0]}
-                            addPlayer={(e:any) => this.addPlayer(0, e)}
-                            changePlayer={(e:any,a:any) => this.changePlayer(0, e,a)} 
-                            removePlayer={(e:any) => this.removePlayer(0, e)}
-                            matchState={this.state.matchState}/>
+                            addPlayer={(e: any) => this.addPlayer(0, e)}
+                            changePlayer={(e: any, a: any) => this.changePlayer(0, e, a)}
+                            removePlayer={(e: any) => this.removePlayer(0, e)}
+                            matchState={this.state.matchState} />
                     </IonSlide>
                     <IonSlide>
                         <TeamCard team="Time B" players={players[1]}
-                            addPlayer={(e:any) => this.addPlayer(1, e)}
-                            changePlayer={(e:any,a:any) => this.changePlayer(1, e,a)}
-                            removePlayer={(e:any) => this.removePlayer(1, e)}
+                            addPlayer={(e: any) => this.addPlayer(1, e)}
+                            changePlayer={(e: any, a: any) => this.changePlayer(1, e, a)}
+                            removePlayer={(e: any) => this.removePlayer(1, e)}
                             matchState={this.state.matchState} />
                     </IonSlide>
                     <IonSlide>
                         <Statics infoPlayers={this.state.infoPlayers} />
                     </IonSlide>
                 </IonSlides>
-                <ClockOptions setState={(e:any) => this.setState(e)} />
+                <ClockOptions setState={(e: any) => this.setState(e)} />
             </div>
         )
     }
--- frontend/src/firebase/firestore.js
+++ frontend/src/firebase/firestore.js
@@ -3,107 +3,162 @@ import { useState } from 'react';
 import { key } from 'ionicons/icons';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBUVobQ91KDm5Fv0wTbAsAy4arysw31Mgs",
-    authDomain: "cupmanager-57723.firebaseapp.com",
-    databaseURL: "https://cupmanager-57723.firebaseio.com",
-    projectId: "cupmanager-57723",
-    storageBucket: "cupmanager-57723.appspot.com",
-    messagingSenderId: "381757034631",
-    appId: "1:381757034631:web:685fd9c9b88951e43b2822",
-    measurementId: "G-QLG6ZHPBV2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  const db = firebase.firestore();
-  const userCollection = "users/cCRmcIVZKs0ru6yREORe/Cup/"
-  const cupCollection = db.collection(userCollection)
+  apiKey: "AIzaSyBUVobQ91KDm5Fv0wTbAsAy4arysw31Mgs",
+  authDomain: "cupmanager-57723.firebaseapp.com",
+  databaseURL: "https://cupmanager-57723.firebaseio.com",
+  projectId: "cupmanager-57723",
+  storageBucket: "cupmanager-57723.appspot.com",
+  messagingSenderId: "381757034631",
+  appId: "1:381757034631:web:685fd9c9b88951e43b2822",
+  measurementId: "G-QLG6ZHPBV2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebase.firestore();
+const userCollection = "users/cCRmcIVZKs0ru6yREORe/Cup/"
+const cupCollection = db.collection(userCollection)
 
-  export async function loginUser() {
-    const isUserLogin = await getUserLogin()
-    var res = []
-    var aux = 1
-    if (!isUserLogin) {
-      await firebase.auth().signInAnonymously().catch(function(error) {
-        const _error = error.message
-        console.log(_error)
-        res.push({'Error': _error})
-        aux = 0
-      });
-      var user = firebase.auth().currentUser
-      console.log('UID --> '+ user.uid)
-    }
-    return res
+export async function loginUser() {
+  const isUserLogin = await getUserLogin()
+  var res = []
+  var aux = 1
+  if (!isUserLogin) {
+    await firebase.auth().signInAnonymously().catch(function (error) {
+      const _error = error.message
+      console.log(_error)
+      res.push({ 'Error': _error })
+      aux = 0
+    });
+    var user = firebase.auth().currentUser
+    console.log('UID --> ' + user.uid)
   }
+  return res
+}
 
-  export async function getUserLogin() {
-    var user = firebase.auth().currentUser;
-    if (user == null) {
-      return false
-    } else {
-      return true
-    }
-  }
-
-  export async function postCup(cupName) {
-    cupName = cupName.replace("/", "")
-    const res = await getCup(cupName)
-    if (res) return false;
-    await cupCollection.doc(cupName).set({
-      idMatches: [],
-      idPlayers: [],
-      name: cupName
-    })
+export async function getUserLogin() {
+  var user = firebase.auth().currentUser;
+  if (user == null) {
+    return false
+  } else {
     return true
   }
+}
 
-  export async function getCup(cupName) {
-    var result = false;
-    await db.collection(userCollection).where("name","==",cupName).get()
-        .then(snapshot=> {
-          snapshot.forEach(doc => {
-            const data = doc.data()
-            console.log(data)
-            if (data.name == cupName) {
-              result = true
-            }
+export async function postCup(cupName) {
+  cupName = cupName.replace("/", "")
+  const res = await getCup(cupName)
+  if (res) return false;
+  await cupCollection.doc(cupName).set({
+    idMatches: [],
+    idPlayers: [],
+    name: cupName
+  })
+  return true
+}
+
+export async function getCup(cupName) {
+  var result = false;
+  await db.collection(userCollection).doc(cupName).get()
+    .then(cup => {
+      if (!cup.exists) console.log(`${cup} não existe`)
+      else console.log(cup.data())
+    })
+  /*await db.collection(userCollection).where("name","==",cupName).get()
+      .then(snapshot=> {
+        snapshot.forEach(doc => {
+          const data = doc.data()
+          console.log(data)
+          if (data.name == cupName) {
+            result = true
+          }
+        })
+      })
+  return result */
+}
+export async function postMatch(cupName, matchName, matchData) {
+  let resp = undefined;
+  await db.collection('cups').doc(cupName).collection('matches').doc(matchName).set(matchData)
+    .then((succ) => { resp = `success` })
+    .catch((err) => { resp = `error ${err}` })
+  return resp;
+}
+
+export async function getMatch(cupName, matchName) {
+  let resp = undefined;
+  await db.collection('cups').doc(cupName).collection('matches').doc(matchName).get()
+    .then((match) => {
+      if (match.exists) resp = match.data()
+    })
+    .catch(err => resp = err)
+  return resp
+}
+
+export async function putMatch(cupName, matchName, matchData) {
+  let resp = undefined;
+  await db.collection('cups').doc(cupName).collection('matches').doc(matchName).set(matchData)
+    .then((succ) => { resp = `success` })
+    .catch((err) => { resp = `error ${err}` })
+  return resp;
+}
+
+export async function getPlayer(cupName, playerName) {
+  let resp = undefined;
+  await db.collection('cups').doc(cupName).collection('players').doc(playerName).get()
+    .then((player) => {
+      if (player.exists) resp = player.data()
+    })
+    .catch(err => resp = err)
+  return resp
+}
+
+export async function getPlayers(cupName, param, crit, val) {
+  let resp = [];
+  if ((crit && param && val) != undefined) {
+    await db.collection('cups').doc(cupName).collection('players').where(param, crit, val)
+      .get().then(players => {
+        if (!players.empty) players.forEach(player => resp.push(players))
+      })
+  }
+  else {
+    await db.collection('cups').doc(cupName).collection('players').get().then(players => {
+      if (!players.empty) players.forEach(player => resp.push(players))
+    })
+  }
+  return resp;
+}
+
+export async function getMatches(keyCup) {
+  const list = []
+  if (keyCup != "" && keyCup != undefined) {
+    keyCup = keyCup.replace("/", "")
+    await db.collection(userCollection + keyCup + "/Match").orderBy("idMatch", "asc").get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const { goalsA, goalsB, idMatch, idPlayersA, idPlayersB, winner } = doc.data()
+          list.push({
+            goalsB,
+            goalsA,
+            idMatch,
+            idPlayersA,
+            idPlayersB,
+            winner
           })
         })
-    return result
-  }
-
-  export async function getMatches(keyCup) {
-    const list =[]
-    if (keyCup != "" && keyCup != undefined) {
-      keyCup = keyCup.replace("/", "")
-      await db.collection(userCollection+keyCup+"/Match").orderBy("idMatch", "asc").get()
-          .then(snapshot=> {
-            snapshot.forEach(doc => {
-              const {goalsA,goalsB,idMatch,idPlayersA,idPlayersB,winner} = doc.data()
-              list.push({
-                goalsB,
-                goalsA,
-                idMatch,
-                idPlayersA,
-                idPlayersB,
-                winner
-              })
-            })
       })
-    }
-    console.log(list)
-    return list
   }
+  console.log(list)
+  return list
+}
 
-  export async function deleteMatche(idMatch, keyCup) {
-    keyCup = keyCup.replace("/", "")
-    idMatch = idMatch.toString()
-    console.log(keyCup + " " + idMatch)
-    await db.collection(userCollection+keyCup+"/Match").doc(idMatch).delete().then(function() {
-      console.log("Partida apagada!")
-    }).catch(function(error) {
-      console.log("Error nessa porra: ",error)
-    }) 
-  }
+export async function deleteMatche(idMatch, keyCup) {
+  keyCup = keyCup.replace("/", "")
+  idMatch = idMatch.toString()
+  console.log(keyCup + " " + idMatch)
+  await db.collection(userCollection + keyCup + "/Match").doc(idMatch).delete().then(function () {
+    console.log("Partida apagada!")
+  }).catch(function (error) {
+    console.log("Error nessa porra: ", error)
+  })
+}
 
-  
